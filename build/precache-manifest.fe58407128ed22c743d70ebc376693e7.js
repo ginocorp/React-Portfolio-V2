5913d8d1423b1420503fb7bd3b88af05f6777877
Gino Colman <gdcolman95@gmail.com>
2022-03-31 18:17:57 -0400

    build folder published

---
+++ build/precache-manifest.fe58407128ed22c743d70ebc376693e7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ef79e0147abd0743223453dda94b2d",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "fd3163181848953f5cba",
+    "url": "/react-portfolio/static/css/2.c11e63b2.chunk.css"
+  },
+  {
+    "revision": "71bc8d0c591d450aefd3",
+    "url": "/react-portfolio/static/css/main.bc2e0710.chunk.css"
+  },
+  {
+    "revision": "fd3163181848953f5cba",
+    "url": "/react-portfolio/static/js/2.8f71091a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.8f71091a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71bc8d0c591d450aefd3",
+    "url": "/react-portfolio/static/js/main.569dc98b.chunk.js"
+  },
+  {
+    "revision": "20bebc02f4cb5515d2ff",
+    "url": "/react-portfolio/static/js/runtime-main.cd07675b.js"
+  },
+  {
+    "revision": "4bb4d8044e0f91cc74a80e6260f37fe4",
+    "url": "/react-portfolio/static/media/pic1.4bb4d804.jpg"
+  },
+  {
+    "revision": "f8a7aa0c5e01bcf6f78807ffb6c190f2",
+    "url": "/react-portfolio/static/media/pic3.f8a7aa0c.jpg"
+  },
+  {
+    "revision": "32f54dd8100496fbe868b944ada14908",
+    "url": "/react-portfolio/static/media/profilepic.32f54dd8.jpg"
+  }
+]);
